a1e4aa0af8831e7646f529509266e36b78a5ab01
Clément Habinshuti <hello@habbes.xyz>
2020-03-12 11:58:01 +0300

    Setup basic projects and test il generation

---
+++ src/Habbes.Sperse.Compiler/Compiler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection.Emit;
+
+namespace Habbes.Sperse.Compiler
+{
+    public class Compiler
+    {
+        public static Func<int, int> GenerateMethod(string name, int multiplier)
+        {
+            var method = new DynamicMethod(
+                name,
+                typeof(int),
+                new[] { typeof(int) });
+
+            var il = method.GetILGenerator();
+
+            il.Emit(OpCodes.Ldarg_0);
+            il.Emit(OpCodes.Ldc_I4, multiplier);
+            il.Emit(OpCodes.Mul);
+            il.Emit(OpCodes.Ret);
+
+            var fn = method.CreateDelegate(typeof(Func<int, int>)) as Func<int, int>;
+            return fn;
+        }
+    }
+}
---
+++ src/Habbes.Sperse.SampleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Habbes.Sperse.Compiler;
+
+namespace Habbes.Sperse.SampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = Compiler.Compiler.GenerateMethod("Double", 2);
+            var quad = Habbes.Sperse.Compiler.Compiler.GenerateMethod("Quadruple", 4);
+
+            Console.WriteLine("Result of double 10 = {0}", db(10));
+            Console.WriteLine("Result of quadruple 10 = {0}", quad(10));
+        }
+    }
+}
